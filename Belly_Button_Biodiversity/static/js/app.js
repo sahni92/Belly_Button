ceae20b74dd06c76383630bfce0843c40f922e09
Mansi Sahni <sahni92@gmail.com>
2019-06-10 20:18:05 -0500

    Cleaning it up

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -18,20 +18,7 @@ function buildMetadata(sample) {
     });
   });
 }
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-
+ 
 
 function buildCharts(sample) {
 
@@ -84,15 +71,6 @@ function buildCharts(sample) {
     });
   });   
 }
-
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-
 
 function init() {
  
